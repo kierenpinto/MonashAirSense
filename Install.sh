22056f57539de9120f8ca0c52d269547416007a1
Kieren Pinto <kieren.pinto@hotmail.com>
2019-02-26 12:03:59 +1100

    Partial Change to Install Script RUN script modified to work

--- Install.sh
+++ Install.sh
@@ -3,3 +3,6 @@
 #Place startup script in /etc/rc.local as 
 #       /root/MonashAirSense/RUN Start
 # or.... $this directory/RUN Start
+
+pip install paho-mqtt
+/root/MonashAirSense/RUN Start >> /etc/rc.local
